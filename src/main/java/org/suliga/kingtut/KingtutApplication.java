e62f23a5e04ba079d44d526f92c7949e22a89f16
tom <tom2wheeler@gmail.com>
2017-05-19 17:13:40 -0700

    Initial commit of king java tutorial spring boot project

---
+++ src/main/java/org/suliga/kingtut/KingtutApplication.java
@@ -0,0 +1,12 @@
+package org.suliga.kingtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingtutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingtutApplication.class, args);
+	}
+}
